e0a91fa9bd51277ba1c11944f7abbf4e1e52d5e8
inakoide <92826468+inakoide@users.noreply.github.com>
2021-11-09 15:52:57 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+function preload(){
+  teapot=loadModel("yourMesh.stl",true);
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight,WEBGL);
+}
+
+function draw() {
+  background(255);
+  push();
+  translate(-width/2,-height/2);
+  fill(255,255,0);
+  pop();
+  push();
+  scale(3);
+  rotateX(PI/2);
+  rotateZ(frameCount*0.01);
+  noStroke();
+  ambientLight(80);
+  directionalLight(0,0,255,0,1,0);
+  model(teapot);
+  pop();
+  
+}
